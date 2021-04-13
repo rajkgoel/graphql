6494824dd62c44bacd8e9a8f419afa4157f5f9aa
Raj Goel <rajkgoel@gmail.com>
2021-04-13 21:52:09 +0530

    Some code refactoring and reworked the Company Detail page.

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,51 +1,63 @@
 const endPointURL = 'http://localhost:9000/graphql'
 
 export async function loadJobs() {
-    const response = await fetch(endPointURL, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            query: `{
-                jobs {
+    const query = `
+        {
+            jobs {
+                id,
+                title,
+                company {
                     id,
-                    title,
-                    company {
-                        id,
-                        name
-                    }
+                    name
                 }
-            }`
-        })
-    });
-
-    const {data} = await response.json();
-    return data.jobs;
+            }
+        }`
+    const { jobs } = await graphqlRequest(query)
+    return jobs;
 }
 
 export async function loadJob(id) {
+    const query = `
+        query JobQuery($id: ID!){
+            job(id: $id) {
+                id
+                title,
+                description,
+                company {
+                    id,
+                    name
+                }
+            }
+        }`
+    const { job } = await graphqlRequest(query, { id });
+    return job;
+}
+
+export async function loadCompany(id) {
+    const query = `query CompanyQuery($id: ID!){
+        company(id: $id) {
+            id
+            name,
+            description
+        }
+    }`
+    const { company } = await graphqlRequest(query, { id });
+    return company;
+}
+
+export async function graphqlRequest(query, variables = {}) {
     const response = await fetch(endPointURL, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
-        body: JSON.stringify({
-            query: `query JobQuery($id: ID!){
-                job(id: $id) {
-                  id
-                  title,
-                  description,
-                  company {
-                    id,
-                    name
-                  }
-                }
-            }`,
-            variables: {id}
-        })
+        body: JSON.stringify({ query, variables })
     });
 
-    const {data} = await response.json();
-    return data.job;
+    if (response.errors) {
+        const message = response.errors.map((e) => e.message).join('\n');
+        throw new Error(message);
+    }
+    const { data } = await response.json();
+    return data;
 }
