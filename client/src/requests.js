ec2195d7a12cdf813f9aa1571ac1f748a57ce03c
Raj Goel <rajkgoel@gmail.com>
2021-04-27 06:44:02 +0530

    Cache the data using ApolloClient. Update Cache using CreateJob result, so that no new fetch need to be requested again.

--- client/src/requests.js
+++ client/src/requests.js
@@ -36,28 +36,33 @@ export async function loadJobs() {
     }`;
 
     try {
-        const { data: { jobs } } = await client.query({ query });
-        console.log(jobs);
+        const { data: { jobs } } = await client.query({ 
+            query, 
+            fetchPolicy: 'no-cache'
+         });
         return jobs;
     } catch (error) {
         console.log(error);
     }
 }
 
-export async function loadJob(id) {
-    const query = gql`
-        query JobQuery($id: ID!){
-            job(id: $id) {
+const jobQuery = gql`
+    query JobQuery($id: ID!){
+        job(id: $id) {
+            id
+            title
+            description
+            company {
                 id
-                title,
-                description,
-                company {
-                    id,
-                    name
-                }
+                name
             }
-        }`;
-    const {data: {job}} = await client.query({query, variables: { id }});
+        }
+    }`;
+
+export async function loadJob(id) {
+    const {data: {job}} = await client.query({
+        query: jobQuery, 
+        variables: { id }});
     return job;
 }
 
@@ -74,29 +79,30 @@ export async function loadCompany(id) {
                 }
             }
     }`;
-    const { data: { company }} = await client.query({query, variables: { id }})
+    const { data: { company }} = await client.query({
+        query, 
+        variables: { id }
+    })
     return company;
 }
 
-export async function graphqlRequest(query, variables = {}) {
-    const request = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify({ query, variables })
-    }
-
-    if (isLoggedIn()) {
-        request.headers['authorization'] = `Bearer ${getAccessToken()}`
-    }
-    const response = await fetch(endPointURL, request);
-
-    if (response.errors) {
-        const message = response.errors.map((e) => e.message).join('\n');
-        throw new Error(message);
-    }
-    const { data } = await response.json();
-    return data;
-}
+// export async function graphqlRequest(query, variables = {}) {
+//     const request = {
+//         method: 'POST',
+//         headers: { 'content-type': 'application/json' },
+//         body: JSON.stringify({ query, variables })
+//     }
+//     if (isLoggedIn()) {
+//         request.headers['authorization'] = `Bearer ${getAccessToken()}`
+//     }
+//     const response = await fetch(endPointURL, request);
+//     if (response.errors) {
+//         const message = response.errors.map((e) => e.message).join('\n');
+//         throw new Error(message);
+//     }
+//     const { data } = await response.json();
+//     return data;
+// }
 
 export async function createJob(input) {
     const mutation = gql`
@@ -111,6 +117,16 @@ export async function createJob(input) {
                 }
             }
         }`;
-    const { data: { job }} = await client.mutate({mutation, variables: { input }});
+    const { data: { job }} = await client.mutate({
+        mutation, 
+        variables: { input },
+        update: (cache, {data}) => {
+            cache.writeQuery({ 
+                query: jobQuery, 
+                variables: { id: data.job.id },
+                data
+            })
+        }
+    });
     return job;
 }
