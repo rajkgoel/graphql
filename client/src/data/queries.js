6bb6465d16bb595a7aa978ae0d22ceecd91a74b1
Raj Goel <rajkgoel@gmail.com>
2021-04-28 07:01:50 +0530

    Applied query fragments

--- client/src/data/queries.js
+++ client/src/data/queries.js
@@ -1,41 +1,42 @@
 import { gql } from "graphql-tag";
 
+const jobDetailFragment = gql`
+    fragment JobDetail on Job {
+        id
+        title
+        description
+        company {
+            id
+            name
+        }
+    }
+`;
+
 export const jobQuery = gql`
     query JobQuery($id: ID!){
         job(id: $id) {
-            id
-            title
-            description
-            company {
-                id
-                name
-            }
+            ...JobDetail
         }
-    }`;
+    }
+    ${jobDetailFragment}
+`;
 
 export const loadJobsQuery = gql`{
         jobs {
-            id
-            title
-            company {
-                id
-                name
-            }
+            ...JobDetail
         }
-    }`;
+    }
+    ${jobDetailFragment}
+`;
 
 export const createJobMutation = gql`
     mutation CreateJob($input: CreateJobInput) {
         job: createJob(input: $input) {
-            id
-            title
-            description
-            company {
-                id
-                name
-            }
+            ...JobDetail
         }
-    }`;
+    }
+    ${jobDetailFragment}
+`;
 
 export const loadCompanyQuery = gql`
     query CompanyQuery($id: ID!){
@@ -48,4 +49,5 @@ export const loadCompanyQuery = gql`
                 title
             }
         }
-    }`;
+    }
+`;
